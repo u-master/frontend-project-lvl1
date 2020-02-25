54ce4213b9a3368d6acdd76c04661324240150d3
u-master <to-umaster@mail.ru>
2020-02-25 15:36:52 +0600

    add eslinter, check everythitg by linter

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@
 import readlineSync from 'readline-sync';
 
 const main = () => {
-    console.log('Welcome to the Brain Games!\n');
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}!`);
-}
+  console.log('Welcome to the Brain Games!\n');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+};
 
 export default main;
