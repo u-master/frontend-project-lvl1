c0358220af586cd34061e8758546cb58d2986424
u-master <to-umaster@mail.ru>
2020-03-12 16:45:56 +0600

    optimize question generation

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,17 +1,15 @@
 
-import { play } from '../engine.js';
+import play from '../engine.js';
 import generateNumber from '../generateNumber.js';
 
-const makeProgressionString = (progression, position) => progression.map(
-  (e, i) => (i === position ? '..' : String(e)),
+const getQuestion = (progression, indexHidden) => progression.map(
+  (e, i) => (i === indexHidden ? '..' : String(e)),
 ).join(' ');
 
 const makeProgression = (start, step, length) => {
   const progression = [];
-  let currentValue = start;
   for (let i = 0; i < length; i += 1) {
-    progression.push(currentValue);
-    currentValue += step;
+    progression.push(start + i * step);
   }
   return progression;
 };
@@ -21,10 +19,10 @@ const generateRound = () => {
   const step = generateNumber(1, 20);
   const length = 10;
   const progression = makeProgression(start, step, length);
-  const position = generateNumber(0, length - 1);
+  const indexHidden = generateNumber(0, length - 1);
   return {
-    question: makeProgressionString(progression, position),
-    answer: String(progression[position]),
+    question: getQuestion(progression, indexHidden),
+    answer: String(progression[indexHidden]),
   };
 };
 
